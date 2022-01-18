de99a6cf41cabc4cc0dbfa52a0f95a67c64c2f52
jdominiclobo <jdominiclobo@gmail.com>
2022-01-18 19:50:15 +0530

    responsive navbar setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-container container">
+          <Link to="/" className="navbar-logo">
+            Website
+          </Link>
+          <div className="menu-icon">{}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
