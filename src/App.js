91cedeb8024036e38d4ceb1b49dd839f1c4dc54c
jdominiclobo <jdominiclobo@gmail.com>
2022-01-19 23:42:08 +0530

    homepage data defining

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/HomePage/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Home />
       <Routes>
-        <Route to="/" element={Navbar} />
+        <Route path="/" exact element={Home} />
       </Routes>
     </BrowserRouter>
   );
